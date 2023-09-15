f4df7da4a621b3e7ab6071c3737786459cba9131
Santy <sanpiquin@gmail.com>
2023-09-15 06:55:21 -0300

    moving the dataBase proyect to another repo

--- dataBase-managment/src/hooks/useUsers.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { UsersContext } from '../contexts/usersContext'
-
-export function useUsers () {
-  const { users, refreshUsers } = useContext(UsersContext)
-
-  return ({ users, refreshUsers })
-}
--- dataBase-managment/src/services/filterUsers.js
+++
@@ -1,5 +0,0 @@
-export function filterUsers ({ users, search }) {
-  return users.filter(user => {
-    return (user.apellido.toUpperCase().startsWith(search.toUpperCase()))
-  })
-}
